5ca20bc3e5db11c7ed5ea927a962796b40c89c36
Madot <migueldeolim1@gmail.com>
2018-03-10 23:26:34 -0400

    Comment

--- js/Notification.js
+++ js/Notification.js
@@ -15,6 +15,6 @@ function changeServiceWorkerFilePath() {
 };
 
 OneSignal.push(function () {
-    changeServiceWorkerFilePath();
+    //changeServiceWorkerFilePath();
     OneSignal.init(initOptions);
 });
