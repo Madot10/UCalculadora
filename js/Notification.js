9df6fd557ac1e39f05dc21e19b2997575aa7226e
Madot <migueldeolim1@gmail.com>
2018-03-10 22:42:34 -0400

    Notification System Add V1.0

---
+++ js/Notification.js
@@ -0,0 +1,19 @@
+window.OneSignal = window.OneSignal || [];
+
+
+function changeServiceWorkerFilePath() {
+    OneSignal.SERVICE_WORKER_PATH = 'UCalculadora/OneSignalSDKWorker.js';
+    OneSignal.SERVICE_UPDATER_WORKER_PATH = 'UCalculadora/OneSignalSDKUpdaterWorker.js';
+    OneSignal.SERVICE_WORKER_PARAM = { scope: '/UCalculadora/' };
+};
+
+OneSignal.push(function () {
+    changeServiceWorkerFilePath();
+    OneSignal.init({
+        appId: "5bc53e0e-df54-43b0-9da3-90efd72057ad",
+        autoRegister: false,
+        notifyButton: {
+          enable: true,
+        },
+      });
+});
