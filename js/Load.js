0e27a68b6c02e7b6873569072b768d347bebf439
Madot <migueldeolim1@gmail.com>
2018-03-25 21:39:45 -0400

    Revert "Revert "Load System""
    This reverts commit c829333460739850f2d1a57699ff467cc06c599b.

---
+++ js/Load.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+
+    //divS
+    let allDiv = document.getElementById("container");
+    let loadDiv = document.getElementsByClassName("loader")[0];
+
+    loadDiv.style.display = "none";
+    allDiv.style.display = "block";
+}
