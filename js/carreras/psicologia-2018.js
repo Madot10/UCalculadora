18996075898fb0e36613392f327102fdb1ec583a
Madot <migueldeolim1@gmail.com>
2018-08-03 23:18:57 -0400

    Fix psico

--- js/carreras/psicologia-2018.js
+++ js/carreras/psicologia-2018.js
@@ -1,1 +1,516 @@
-var psicologia=[{ 	"Semestre": "PRIMER SEMESTRE", 	"Asignatura": "Comprensión y Producción de Textos Escritos", 	"UC": 5, 	"Tax": "TA‐6" }, { 	"Semestre": "PRIMER SEMESTRE", 	"Asignatura": "Identidad, Liderazgo y Compromiso I", 	"UC": 3, 	"Tax": "TA‐1" }, { 	"Semestre": "PRIMER SEMESTRE", 	"Asignatura": "Historia y Fundamentos Filosóficos de la Psicología", 	"UC": 5, 	"Tax": "TA‐1" }, { 	"Semestre": "PRIMER SEMESTRE", 	"Asignatura": "Introducción al Estudio de la Psicología", 	"UC": 5, 	"Tax": "TA‐1" }, { 	"Semestre": "PRIMER SEMESTRE", 	"Asignatura": "Introducción a la Investigación", 	"UC": 5, 	"Tax": "TA‐1" }, { 	"Semestre": "PRIMER SEMESTRE", 	"Asignatura": "Psicobiología", 	"UC": 3, 	"Tax": "TA‐1" }, { 	"Semestre": "PRIMER SEMESTRE", 	"Asignatura": "Matemática y Estadística", 	"UC": 7, 	"Tax": "TA‐5" }, { 	"Semestre": "SEGUNDO SEMESTRE", 	"Asignatura": "Identidad, Liderazgo y Compromiso II", 	"UC": 3, 	"Tax": "TA‐1" }, { 	"Semestre": "SEGUNDO SEMESTRE", 	"Asignatura": "Ecología, Ambiente y Sustentabilidad", 	"UC": 3, 	"Tax": "TA‐1" }, { 	"Semestre": "SEGUNDO SEMESTRE", 	"Asignatura": "Neurociencias I", 	"UC": 5, 	"Tax": "TA‐1" }, { 	"Semestre": "SEGUNDO SEMESTRE", 	"Asignatura": "Teorías y Sistemas en Psicología", 	"UC": 8, 	"Tax": "TA‐1" }, { 	"Semestre": "SEGUNDO SEMESTRE", 	"Asignatura": "Investigación Documental", 	"UC": 2, 	"Tax": "TA‐8" }, { 	"Semestre": "SEGUNDO SEMESTRE", 	"Asignatura": "Estadística I", 	"UC": 7, 	"Tax": "TA‐5" }, { 	"Semestre": "TERCER SEMESTRE", 	"Asignatura": "Psicología de la Sensopercepción y de la Atención", 	"UC": 5, 	"Tax": "TA‐1" }, { 	"Semestre": "TERCER SEMESTRE", 	"Asignatura": "Psicología de la Motivación y de la Emoción", 	"UC": 5, 	"Tax": "TA‐1" }, { 	"Semestre": "TERCER SEMESTRE", 	"Asignatura": "Neurociencias II", 	"UC": 5, 	"Tax": "TA‐1" }, { 	"Semestre": "TERCER SEMESTRE", 	"Asignatura": "Cultura, Sociedad e Individuo", 	"UC": 3, 	"Tax": "TA‐1" }, { 	"Semestre": "TERCER SEMESTRE", 	"Asignatura": "Psicología de la Memoria", 	"UC": 5, 	"Tax": "TA‐1" }, { 	"Semestre": "TERCER SEMESTRE", 	"Asignatura": "Observación y Entrevista", 	"UC": 3, 	"Tax": "TA‐8" }, { 	"Semestre": "TERCER SEMESTRE", 	"Asignatura": "Estadística II", 	"UC": 7, 	"Tax": "TA‐5" }, { 	"Semestre": "CUARTO SEMESTRE", 	"Asignatura": "Psicología del Aprendizaje", 	"UC": 5, 	"Tax": "TA‐1" }, { 	"Semestre": "CUARTO SEMESTRE", 	"Asignatura": "Neurociencias Cognitiva y Neuropsicología", 	"UC": 5, 	"Tax": "TA‐1" }, { 	"Semestre": "CUARTO SEMESTRE", 	"Asignatura": "Investigación Cualitativa", 	"UC": 5, 	"Tax": "TA‐1" }, { 	"Semestre": "CUARTO SEMESTRE", 	"Asignatura": "Psicología Social I", 	"UC": 3, 	"Tax": "TA‐1" }, { 	"Semestre": "CUARTO SEMESTRE", 	"Asignatura": "Investigación Cuantitativa No Experimental", 	"UC": 5, 	"Tax": "TA‐1" }, { 	"Semestre": "CUARTO SEMESTRE", 	"Asignatura": "Psicometría", 	"UC": 8, 	"Tax": "TA‐3" }, { 	"Semestre": "QUINTO SEMESTRE", 	"Asignatura": "Psicología del Pensamiento y del Lenguaje", 	"UC": 7, 	"Tax": "(V) TA‐1" }, { 	"Semestre": "QUINTO SEMESTRE", 	"Asignatura": "Psicología de la Inteligencia", 	"UC": 7, 	"Tax": "(V) TA‐1" }, { 	"Semestre": "QUINTO SEMESTRE", 	"Asignatura": "Psicología del Desarrollo de la Niñez", 	"UC": 5, 	"Tax": "TA‐1" }, { 	"Semestre": "QUINTO SEMESTRE", 	"Asignatura": "Psicología Social II", 	"UC": 5, 	"Tax": "TA‐1" }, { 	"Semestre": "QUINTO SEMESTRE", 	"Asignatura": "Antropología Filosófica", 	"UC": 2, 	"Tax": "TA‐8" }, { 	"Semestre": "QUINTO SEMESTRE", 	"Asignatura": "Psicología Experimental", 	"UC": 5, 	"Tax": "TA‐1" }, { 	"Semestre": "QUINTO SEMESTRE", 	"Asignatura": "Evaluación Psicológica I", 	"UC": 7, 	"Tax": "TA‐2" }, { 	"Semestre": "SEXTO SEMESTRE", 	"Asignatura": "Psicología de la Personalidad", 	"UC": 8, 	"Tax": "TA‐1" }, { 	"Semestre": "SEXTO SEMESTRE", 	"Asignatura": "Psicología del Desarrollo de la Adolescencia y de la Adultez", 	"UC": 5, 	"Tax": "TA‐1" }, { 	"Semestre": "SEXTO SEMESTRE", 	"Asignatura": "Pasantías en Psicología Social", 	"UC": 2, 	"Tax": "TA‐7" }, { 	"Semestre": "SEXTO SEMESTRE", 	"Asignatura": "Psicología Escolar", 	"UC": 6, 	"Tax": "TA‐1" }, { 	"Semestre": "SEXTO SEMESTRE", 	"Asignatura": "Psicopatología de la Niñez y La Adolescencia", 	"UC": 5, 	"Tax": "TA‐1" }, { 	"Semestre": "SEXTO SEMESTRE", 	"Asignatura": "Evaluación Psicológica II", 	"UC": 5, 	"Tax": "TA‐2" }, { 	"Semestre": "SEXTO SEMESTRE", 	"Asignatura": "Prácticas de Psicología Experimental", 	"UC": 2, 	"Tax": "TA‐9" }, { 	"Semestre": "SÉPTIMO SEMESTRE", 	"Asignatura": "Psicopatología de la Adultez", 	"UC": 5, 	"Tax": "TA‐1" }, { 	"Semestre": "SÉPTIMO SEMESTRE", 	"Asignatura": "Evaluación Psicológica III", 	"UC": 7, 	"Tax": "TA‐2" }, { 	"Semestre": "SÉPTIMO SEMESTRE", 	"Asignatura": "Investigación Mixta", 	"UC": 7, 	"Tax": "(V) TA‐1" }, { 	"Semestre": "SÉPTIMO SEMESTRE", 	"Asignatura": "Pasantías de Psicología Escolar I", 	"UC": 6, 	"Tax": "TA‐7" }, { 	"Semestre": "SÉPTIMO SEMESTRE", 	"Asignatura": "Gestión de Proyectos", 	"UC": 5, 	"Tax": "TA‐8" }, { 	"Semestre": "SÉPTIMO SEMESTRE", 	"Asignatura": "Electiva", 	"UC": 3, 	"Tax": "TA‐9" }, { 	"Semestre": "OCTAVO SEMESTRE", 	"Asignatura": "Familia e Intervención Familiar", 	"UC": 5, 	"Tax": "TA‐1" }, { 	"Semestre": "OCTAVO SEMESTRE", 	"Asignatura": "Evaluación Psicológica IV", 	"UC": 7, 	"Tax": "TA‐2" }, { 	"Semestre": "OCTAVO SEMESTRE", 	"Asignatura": "Trabajo de Grado I", 	"UC": 5.6, 	"Tax": "(V) TA‐1" }, { 	"Semestre": "OCTAVO SEMESTRE", 	"Asignatura": "Psicología Clínica de la Niñez y Adolescencia", 	"UC": 3, 	"Tax": "TA‐1" }, { 	"Semestre": "OCTAVO SEMESTRE", 	"Asignatura": "Asesoramiento Psicológico", 	"UC": 3, 	"Tax": "TA‐1" }, { 	"Semestre": "OCTAVO SEMESTRE", 	"Asignatura": "Pasantías de Psicología Escolar II", 	"UC": 6, 	"Tax": "TA‐7" }, { 	"Semestre": "OCTAVO SEMESTRE", 	"Asignatura": "Electiva", 	"UC": 3, 	"Tax": "TA‐9" }, { 	"Semestre": "NOVENO SEMESTRE", 	"Asignatura": "Trabajo de Grado II", 	"UC": 2.8, 	"Tax": "(V) TA‐1" }, { 	"Semestre": "NOVENO SEMESTRE", 	"Asignatura": "Psicología Clínica de la Adultez", 	"UC": 3, 	"Tax": "TA‐1" }, { 	"Semestre": "NOVENO SEMESTRE", 	"Asignatura": "Pasantías en Psicología Clínica de la Niñez y Adolescencia", 	"UC": 6, 	"Tax": "TA‐7" }, { 	"Semestre": "NOVENO SEMESTRE", 	"Asignatura": "Pasantías en Asesoramiento Psicológico I", 	"UC": 6, 	"Tax": "TA‐7" }, { 	"Semestre": "NOVENO SEMESTRE", 	"Asignatura": "Psicología del Personal, de las Organizaciones y del Trabajo", 	"UC": 9, 	"Tax": "TA‐4" }, { 	"Semestre": "NOVENO SEMESTRE", 	"Asignatura": "Psicología Crítica", 	"UC": 2, 	"Tax": "TA‐8" }, { 	"Semestre": "NOVENO SEMESTRE", 	"Asignatura": "Electiva", 	"UC": 3, 	"Tax": "TA‐9" }, { 	"Semestre": "DECIMO SEMESTRE", 	"Asignatura": "Ética Profesional", 	"UC": 2, 	"Tax": "TA‐8" }, { 	"Semestre": "DECIMO SEMESTRE", 	"Asignatura": "Psicología Latinoamericana", 	"UC": 4.2, 	"Tax": "(V) TA‐1" }, { 	"Semestre": "DECIMO SEMESTRE", 	"Asignatura": "Trabajo de Grado III", 	"UC": 5.6, 	"Tax": "(V) TA‐1" }, { 	"Semestre": "DECIMO SEMESTRE", 	"Asignatura": "Pasantías en Psicología Clínica de la Adultez", 	"UC": 8, 	"Tax": "TA‐7" }, { 	"Semestre": "DECIMO SEMESTRE", 	"Asignatura": "Pasantías en Asesoramiento Psicológico II", 	"UC": 6, 	"Tax": "TA‐7" }, { 	"Semestre": "DECIMO SEMESTRE", 	"Asignatura": "Electiva", 	"UC": 3, 	"Tax": "TA‐9" }, { 	"Semestre": "DECIMO SEMESTRE", 	"Asignatura": "Electiva", 	"UC": 3, 	"Tax": "TA‐9" }, { 	"Semestre": "SEGUNDO AÑO", 	"Asignatura": "estadística ii", 	"UC": 4, 	"Tax": "TA‐1" }, { 	"Semestre": "TERCER AÑO", 	"Asignatura": "psicología de la personalidad", 	"UC": 4, 	"Tax": "TA‐1" }, { 	"Semestre": "TERCER AÑO", 	"Asignatura": "psicología experimental", 	"UC": 4, 	"Tax": "TA‐1" }, { 	"Semestre": "TERCER AÑO", 	"Asignatura": "psicología general iii", 	"UC": 4, 	"Tax": "TA‐1" }, { 	"Semestre": "TERCER AÑO", 	"Asignatura": "evaluación psicológica i", 	"UC": 6, 	"Tax": "TA‐1" }, { 	"Semestre": "CUARTO AÑO", 	"Asignatura": "psicología escolar", 	"UC": 8, 	"Tax": "TA‐1" }, { 	"Semestre": "CUARTO AÑO", 	"Asignatura": "seminario de trabajo de grado", 	"UC": 1.4, 	"Tax": "(V) TA‐1" }, { 	"Semestre": "CUARTO AÑO", 	"Asignatura": "psicología industrial y organizacional", 	"UC": 6, 	"Tax": "TA‐1" }, { 	"Semestre": "CUARTO AÑO", 	"Asignatura": "evaluación psicológica ii", 	"UC": 6, 	"Tax": "TA‐1" }, { 	"Semestre": "CUARTO AÑO", 	"Asignatura": "psicopatología/clinica i", 	"UC": 4, 	"Tax": "TA‐1" }, { 	"Semestre": "CUARTO AÑO", 	"Asignatura": "electiva i", 	"UC": 1, 	"Tax": "TA‐1" }, { 	"Semestre": "QUINTO AÑO", 	"Asignatura": "ética profesional", 	"UC": 1, 	"Tax": "TA‐1" }, { 	"Semestre": "QUINTO AÑO", 	"Asignatura": "asesoramiento psicológico", 	"UC": 8, 	"Tax": "TA‐1" }, { 	"Semestre": "QUINTO AÑO", 	"Asignatura": "crit. a los sist. psicológicos", 	"UC": 1, 	"Tax": "TA‐1" }, { 	"Semestre": "QUINTO AÑO", 	"Asignatura": "evaluación psicología iii", 	"UC": 2, 	"Tax": "TA‐1" }, { 	"Semestre": "QUINTO AÑO", 	"Asignatura": "psicopatología/clínica ii", 	"UC": 8, 	"Tax": "TA‐1" }, { 	"Semestre": "QUINTO AÑO", 	"Asignatura": "Electiva II", 	"UC": 1, 	"Tax": "TA‐1" }, { 	"Semestre": "QUINTO AÑO", 	"Asignatura": "electiva iii", 	"UC": 1, 	"Tax": "TA‐1" }, { 	"Semestre": "QUINTO AÑO", 	"Asignatura": "trabajo de grado", 	"UC": 5, 	"Tax": "TA‐1" }]
+var psicologia=[{
+	"Semestre": "PRIMER SEMESTRE",
+	"Asignatura": "Comprensión y Producción de Textos Escritos",
+	"UC": 5,
+	"Tax": "TA‐6"
+},
+{
+	"Semestre": "PRIMER SEMESTRE",
+	"Asignatura": "Identidad, Liderazgo y Compromiso I",
+	"UC": 3,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "PRIMER SEMESTRE",
+	"Asignatura": "Historia y Fundamentos Filosóficos de la Psicología",
+	"UC": 5,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "PRIMER SEMESTRE",
+	"Asignatura": "Introducción al Estudio de la Psicología",
+	"UC": 5,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "PRIMER SEMESTRE",
+	"Asignatura": "Introducción a la Investigación",
+	"UC": 5,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "PRIMER SEMESTRE",
+	"Asignatura": "Psicobiología",
+	"UC": 3,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "PRIMER SEMESTRE",
+	"Asignatura": "Matemática y Estadística",
+	"UC": 7,
+	"Tax": "TA‐5"
+},
+{
+	"Semestre": "SEGUNDO SEMESTRE",
+	"Asignatura": "Identidad, Liderazgo y Compromiso II",
+	"UC": 3,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "SEGUNDO SEMESTRE",
+	"Asignatura": "Ecología, Ambiente y Sustentabilidad",
+	"UC": 3,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "SEGUNDO SEMESTRE",
+	"Asignatura": "Neurociencias I",
+	"UC": 5,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "SEGUNDO SEMESTRE",
+	"Asignatura": "Teorías y Sistemas en Psicología",
+	"UC": 8,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "SEGUNDO SEMESTRE",
+	"Asignatura": "Investigación Documental",
+	"UC": 2,
+	"Tax": "TA‐8"
+},
+{
+	"Semestre": "SEGUNDO SEMESTRE",
+	"Asignatura": "Estadística I",
+	"UC": 7,
+	"Tax": "TA‐5"
+},
+{
+	"Semestre": "TERCER SEMESTRE",
+	"Asignatura": "Psicología de la Sensopercepción y de la Atención",
+	"UC": 5,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "TERCER SEMESTRE",
+	"Asignatura": "Psicología de la Motivación y de la Emoción",
+	"UC": 5,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "TERCER SEMESTRE",
+	"Asignatura": "Neurociencias II",
+	"UC": 5,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "TERCER SEMESTRE",
+	"Asignatura": "Cultura, Sociedad e Individuo",
+	"UC": 3,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "TERCER SEMESTRE",
+	"Asignatura": "Investigación Cualitativa",
+	"UC": 5,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "TERCER SEMESTRE",
+	"Asignatura": "Observación y Entrevista",
+	"UC": 3,
+	"Tax": "TA‐8"
+},
+{
+	"Semestre": "TERCER SEMESTRE",
+	"Asignatura": "Estadística II",
+	"UC": 7,
+	"Tax": "TA‐5"
+},
+{
+	"Semestre": "CUARTO SEMESTRE",
+	"Asignatura": "Psicología del Aprendizaje",
+	"UC": 5,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "CUARTO SEMESTRE",
+	"Asignatura": "Psicología de la Memoria",
+	"UC": 5,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "CUARTO SEMESTRE",
+	"Asignatura": "Neurociencias Cognitiva y Neuropsicología",
+	"UC": 5,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "CUARTO SEMESTRE",
+	"Asignatura": "Psicología Social I",
+	"UC": 3,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "CUARTO SEMESTRE",
+	"Asignatura": "Investigación Cuantitativa No Experimental",
+	"UC": 5,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "CUARTO SEMESTRE",
+	"Asignatura": "Psicometría",
+	"UC": 8,
+	"Tax": "TA‐3"
+},
+{
+	"Semestre": "QUINTO SEMESTRE",
+	"Asignatura": "Psicología del Pensamiento y del Lenguaje",
+	"UC": 7,
+	"Tax": "(V) TA‐1"
+},
+{
+	"Semestre": "QUINTO SEMESTRE",
+	"Asignatura": "Psicología de la Inteligencia",
+	"UC": 7,
+	"Tax": "(V) TA‐1"
+},
+{
+	"Semestre": "QUINTO SEMESTRE",
+	"Asignatura": "Psicología del Desarrollo de la Niñez",
+	"UC": 5,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "QUINTO SEMESTRE",
+	"Asignatura": "Psicología Social II",
+	"UC": 5,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "QUINTO SEMESTRE",
+	"Asignatura": "Antropología Filosófica",
+	"UC": 2,
+	"Tax": "TA‐8"
+},
+{
+	"Semestre": "QUINTO SEMESTRE",
+	"Asignatura": "Psicología Experimental",
+	"UC": 5,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "QUINTO SEMESTRE",
+	"Asignatura": "Evaluación Psicológica I",
+	"UC": 7,
+	"Tax": "TA‐2"
+},
+{
+	"Semestre": "SEXTO SEMESTRE",
+	"Asignatura": "Psicología de la Personalidad",
+	"UC": 8,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "SEXTO SEMESTRE",
+	"Asignatura": "Psicología del Desarrollo de la Adolescencia y de la Adultez",
+	"UC": 5,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "SEXTO SEMESTRE",
+	"Asignatura": "Pasantías en Psicología Social",
+	"UC": 2,
+	"Tax": "TA‐7"
+},
+{
+	"Semestre": "SEXTO SEMESTRE",
+	"Asignatura": "Psicología Escolar",
+	"UC": 6,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "SEXTO SEMESTRE",
+	"Asignatura": "Psicopatología de la Niñez y La Adolescencia",
+	"UC": 5,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "SEXTO SEMESTRE",
+	"Asignatura": "Evaluación Psicológica II",
+	"UC": 5,
+	"Tax": "TA‐2"
+},
+{
+	"Semestre": "SEXTO SEMESTRE",
+	"Asignatura": "Prácticas de Psicología Experimental",
+	"UC": 2,
+	"Tax": "TA‐9"
+},
+{
+	"Semestre": "SÉPTIMO SEMESTRE",
+	"Asignatura": "Psicopatología de la Adultez",
+	"UC": 5,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "SÉPTIMO SEMESTRE",
+	"Asignatura": "Evaluación Psicológica III",
+	"UC": 7,
+	"Tax": "TA‐2"
+},
+{
+	"Semestre": "SÉPTIMO SEMESTRE",
+	"Asignatura": "Investigación Mixta",
+	"UC": 7,
+	"Tax": "(V) TA‐1"
+},
+{
+	"Semestre": "SÉPTIMO SEMESTRE",
+	"Asignatura": "Pasantías de Psicología Escolar I",
+	"UC": 6,
+	"Tax": "TA‐7"
+},
+{
+	"Semestre": "SÉPTIMO SEMESTRE",
+	"Asignatura": "Gestión de Proyectos",
+	"UC": 5,
+	"Tax": "TA‐8"
+},
+{
+	"Semestre": "SÉPTIMO SEMESTRE",
+	"Asignatura": "Electiva",
+	"UC": 3,
+	"Tax": "TA‐9"
+},
+{
+	"Semestre": "OCTAVO SEMESTRE",
+	"Asignatura": "Familia e Intervención Familiar",
+	"UC": 5,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "OCTAVO SEMESTRE",
+	"Asignatura": "Evaluación Psicológica IV",
+	"UC": 7,
+	"Tax": "TA‐2"
+},
+{
+	"Semestre": "OCTAVO SEMESTRE",
+	"Asignatura": "Trabajo de Grado I",
+	"UC": 5.6,
+	"Tax": "(V) TA‐1"
+},
+{
+	"Semestre": "OCTAVO SEMESTRE",
+	"Asignatura": "Psicología Clínica de la Niñez y Adolescencia",
+	"UC": 3,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "OCTAVO SEMESTRE",
+	"Asignatura": "Asesoramiento Psicológico",
+	"UC": 3,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "OCTAVO SEMESTRE",
+	"Asignatura": "Pasantías de Psicología Escolar II",
+	"UC": 6,
+	"Tax": "TA‐7"
+},
+{
+	"Semestre": "OCTAVO SEMESTRE",
+	"Asignatura": "Electiva",
+	"UC": 3,
+	"Tax": "TA‐9"
+},
+{
+	"Semestre": "NOVENO SEMESTRE",
+	"Asignatura": "Trabajo de Grado II",
+	"UC": 2.8,
+	"Tax": "(V) TA‐1"
+},
+{
+	"Semestre": "NOVENO SEMESTRE",
+	"Asignatura": "Psicología Clínica de la Adultez",
+	"UC": 3,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "NOVENO SEMESTRE",
+	"Asignatura": "Pasantías en Psicología Clínica de la Niñez y Adolescencia",
+	"UC": 6,
+	"Tax": "TA‐7"
+},
+{
+	"Semestre": "NOVENO SEMESTRE",
+	"Asignatura": "Pasantías en Asesoramiento Psicológico I",
+	"UC": 6,
+	"Tax": "TA‐7"
+},
+{
+	"Semestre": "NOVENO SEMESTRE",
+	"Asignatura": "Psicología del Personal, de las Organizaciones y del Trabajo",
+	"UC": 9,
+	"Tax": "TA‐4"
+},
+{
+	"Semestre": "NOVENO SEMESTRE",
+	"Asignatura": "Psicología Crítica",
+	"UC": 2,
+	"Tax": "TA‐8"
+},
+{
+	"Semestre": "NOVENO SEMESTRE",
+	"Asignatura": "Electiva",
+	"UC": 3,
+	"Tax": "TA‐9"
+},
+{
+	"Semestre": "DECIMO SEMESTRE",
+	"Asignatura": "Ética Profesional",
+	"UC": 2,
+	"Tax": "TA‐8"
+},
+{
+	"Semestre": "DECIMO SEMESTRE",
+	"Asignatura": "Psicología Latinoamericana",
+	"UC": 4.2,
+	"Tax": "(V) TA‐1"
+},
+{
+	"Semestre": "DECIMO SEMESTRE",
+	"Asignatura": "Trabajo de Grado III",
+	"UC": 5.6,
+	"Tax": "(V) TA‐1"
+},
+{
+	"Semestre": "DECIMO SEMESTRE",
+	"Asignatura": "Pasantías en Psicología Clínica de la Adultez",
+	"UC": 8,
+	"Tax": "TA‐7"
+},
+{
+	"Semestre": "DECIMO SEMESTRE",
+	"Asignatura": "Pasantías en Asesoramiento Psicológico II",
+	"UC": 6,
+	"Tax": "TA‐7"
+},
+{
+	"Semestre": "DECIMO SEMESTRE",
+	"Asignatura": "Electiva",
+	"UC": 3,
+	"Tax": "TA‐9"
+},
+{
+	"Semestre": "DECIMO SEMESTRE",
+	"Asignatura": "Electiva",
+	"UC": 3,
+	"Tax": "TA‐9"
+},
+{
+	"Semestre": "SEGUNDO AÑO",
+	"Asignatura": "estadística ii",
+	"UC": 4,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "TERCER AÑO",
+	"Asignatura": "psicología de la personalidad",
+	"UC": 4,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "TERCER AÑO",
+	"Asignatura": "psicología experimental",
+	"UC": 4,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "TERCER AÑO",
+	"Asignatura": "psicología general iii",
+	"UC": 4,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "TERCER AÑO",
+	"Asignatura": "evaluación psicológica i",
+	"UC": 6,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "CUARTO AÑO",
+	"Asignatura": "psicología escolar",
+	"UC": 8,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "CUARTO AÑO",
+	"Asignatura": "seminario de trabajo de grado",
+	"UC": 1.4,
+	"Tax": "(V) TA‐1"
+},
+{
+	"Semestre": "CUARTO AÑO",
+	"Asignatura": "psicología industrial y organizacional",
+	"UC": 6,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "CUARTO AÑO",
+	"Asignatura": "evaluación psicológica ii",
+	"UC": 6,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "CUARTO AÑO",
+	"Asignatura": "psicopatología/clinica i",
+	"UC": 4,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "CUARTO AÑO",
+	"Asignatura": "electiva i",
+	"UC": 1,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "QUINTO AÑO",
+	"Asignatura": "ética profesional",
+	"UC": 1,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "QUINTO AÑO",
+	"Asignatura": "asesoramiento psicológico",
+	"UC": 8,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "QUINTO AÑO",
+	"Asignatura": "crit. a los sist. psicológicos",
+	"UC": 1,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "QUINTO AÑO",
+	"Asignatura": "evaluación psicología iii",
+	"UC": 2,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "QUINTO AÑO",
+	"Asignatura": "psicopatología/clínica ii",
+	"UC": 8,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "QUINTO AÑO",
+	"Asignatura": "Electiva II",
+	"UC": 1,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "QUINTO AÑO",
+	"Asignatura": "electiva iii",
+	"UC": 1,
+	"Tax": "TA‐1"
+},
+{
+	"Semestre": "QUINTO AÑO",
+	"Asignatura": "trabajo de grado",
+	"UC": 5,
+	"Tax": "TA‐1"
+}]
