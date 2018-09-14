27a3002a148b8cce86f317e6ce62b9eaba956ce6
Madot <migueldeolim1@gmail.com>
2018-09-14 14:47:23 -0400

    AyudandoUcab y ActualizacionView

--- js/fun.js
+++ js/fun.js
@@ -47,7 +47,8 @@ let NsusDiv;
 window.onload = function() {
     document.getElementById("sl_carrera").selectedIndex = 0;
     document.getElementById("sl_sede").selectedIndex = 0;
-
+    
+    DiasPasados();
     // Get the modal
     //modal = document.getElementById('myModal');
     //moig = document.getElementById('IgModal');
@@ -80,6 +81,25 @@ window.onload = function() {
     LauchModal('nplus');
 
 }
+
+function DiasPasados() {
+    f1 = '28/02/2018';
+  
+    let today = new Date();
+    f2 = today.getDate() + '/' + (today.getMonth() + 1) + '/' + today.getFullYear();
+  
+    let aFecha1 = f1.split('/');
+    let aFecha2 = f2.split('/');
+  
+    let fFecha1 = Date.UTC(aFecha1[2], aFecha1[1] - 1, aFecha1[0]);
+    let fFecha2 = Date.UTC(aFecha2[2], aFecha2[1] - 1, aFecha2[0]);
+  
+    let dif = fFecha2 - fFecha1;
+    let dias = Math.floor(dif / (1000 * 60 * 60 * 24));
+    console.log(dias);
+    document.getElementById('days').innerHTML = dias;
+  }
+  
 
 //Load Calculadora
 /*
