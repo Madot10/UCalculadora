159da47d6cbe2ff319cb23486453b74eff6184f0
Madot <migueldeolim1@gmail.com>
2019-02-13 22:00:55 -0400

    Calculadora Tool

--- js/main.js
+++ js/main.js
@@ -21,6 +21,7 @@ let limitFab = 30;
 window.onload = () => {
     //Cargamos UC visual
     document.getElementById('ucvalue').innerHTML = `${formatNumber.new(LoadUC())} Bs.S`;
+    UC = visualUC;
 }
 
 /* SISTEMA GENERAL */
