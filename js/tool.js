bf32cba8d2c110644efe9372a0b0b9cbe3242f71
Madot <migueldeolim1@gmail.com>
2019-05-14 20:33:48 -0400

    Implementacion de comportamiento *UC* en tool

--- js/tool.js
+++ js/tool.js
@@ -33,7 +33,35 @@ function cleanAll(){
     document.getElementById('result').innerHTML = '0';
 }
 
+function preformatUC(allOpe){
+    let start = 0;
+    let i = allOpe.indexOf("UC");
+
+    //Encontramos / Verificamos a Der e Izq
+    while(i != -1){
+        i = allOpe.indexOf("UC", start);
+        
+        
+        //DER
+        let der = allOpe.substring(i-1, i);
+        if(der.match(/([0-9])/g)){
+            //Hay numero
+            allOpe = allOpe.substring(0, i) + "*" + allOpe.substring(i, allOpe.length);
+        }
+        //IZQ
+        let izq = allOpe.substring(i+2, i+3);
+        if(izq.match(/([0-9])/g)){
+            //Hay numero
+            allOpe = allOpe.substring(0, i+2) + "*" + allOpe.substring(i+2, allOpe.length+1);
+        }
+        start = i+2;
+    }
+    //console.log(i, allOpe, der.match(/([0-9])/g), izq.match(/([0-9])/g));
+    return allOpe;
+}
+
 function calcular(){
     //console.log(document.getElementsByClassName("cal")[0].textContent);
-    document.getElementById('result').innerHTML = formatNumber.new(eval(document.getElementsByClassName("cal")[0].textContent));
+    let operations = preformatUC(document.getElementsByClassName("cal")[0].textContent);
+    document.getElementById('result').innerHTML = formatNumber.new(eval(operations));
 }
