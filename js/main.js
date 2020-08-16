d37d59d8bbbe8f029ff602d7f10ca9b859cc2a8d
Madot <migueldeolim1@gmail.com>
2020-08-16 19:53:17 -0400

    Postegar cambio de esquema

--- js/main.js
+++ js/main.js
@@ -112,7 +112,7 @@ function InicializarPeriodoSys() {
     if (
         (mesAct > 2 && mesAct < 8) ||
         (mesAct == 2 && diaAct >= 11) /* Mes febrero */ ||
-        (mesAct == 8 && diaAct <= 15) /* Mes Agosto */
+        (mesAct == 8 && diaAct <= 28) /* Mes Agosto */
     ) {
         //PERIODO 1
         //console.warn("PERIODO 1");
