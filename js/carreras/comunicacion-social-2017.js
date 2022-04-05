2c7c2dba6142b9de2804c75409c6290f917c469c
Miguel De Olim <migueldeolim1@gmail.com>
2022-04-04 20:56:34 -0400

    Update comunicacion-social-2017.js

--- js/carreras/comunicacion-social-2017.js
+++ js/carreras/comunicacion-social-2017.js
@@ -315,6 +315,12 @@ var comunicacionsocial = [
 	},
 	{
 		Semestre: "CONCENTRACIÓN PRODUCCIÓN AUDIOVISUAL SEMESTRE A",
+		Asignatura: "Postproducción",
+		UC: 4,
+		Tax: "TA-9",
+	},
+	{
+		Semestre: "CONCENTRACIÓN PRODUCCIÓN AUDIOVISUAL SEMESTRE A",
 		Asignatura: "Historia del Cine",
 		UC: 3,
 		Tax: "TA-1",
